cbe6c9c87c5ab0d96fa0ee4cb00e67a8a202d2bd
Brian Sahota <bpsahota@gmail.com>
2020-09-09 13:36:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,14 @@
 
+# def reverse_each_word(array)
+# new_array = array.split(" ")
+# reversed_array = new_array.each {|x| x.reverse!}
+#   reversed_array.join(" ")
+# end
+
 def reverse_each_word(array)
-new_array = array.split(" ")
-reversed_array = new_array.each {|x| x.reverse!}
-  reversed_array.join(" ")
+  new_array = array.split(" ")
+  new_array.collect {|x| x.reverse!}
+  new_array.join(" ")
 end
-
-
 
 
